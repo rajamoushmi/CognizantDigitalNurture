99342cb08d0c2c8794f118aa8897f7687832b862
DODDA RAJA MOUSHMI <rajamoushmi@gmail.com>
2025-07-05 23:18:10 +0530

    Delete Week3_Sprigng_core_maven/SpringMavenProject directory

--- Week3_Sprigng_core_maven/SpringMavenProject/src/main/java/com/cognizant/springmavenproject/App.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.springmavenproject;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Spring Maven Project is Successfully Configured!");
-    }
-}
