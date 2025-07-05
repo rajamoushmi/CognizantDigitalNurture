db6a8ddc155bf7637adccf5a89790c1d96b6d474
DODDA RAJA MOUSHMI <rajamoushmi@gmail.com>
2025-07-05 23:16:22 +0530

    Creating and Configuring a Maven Project

---
+++ Week3_Sprigng_core_maven/SpringMavenProject/src/main/java/com/cognizant/springmavenproject/App.java
@@ -0,0 +1,7 @@
+package com.cognizant.springmavenproject;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Spring Maven Project is Successfully Configured!");
+    }
+}
